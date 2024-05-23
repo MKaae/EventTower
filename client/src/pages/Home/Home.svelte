956c9c10ffdcd1deb204b5f8c00ea319d3a210c8
Steffen Grøn Andersen <steffengroenandersen@gmail.com>
2024-05-23 20:54:08 +0200

    Add toast

--- client/src/pages/Home/Home.svelte
+++ client/src/pages/Home/Home.svelte
@@ -3,8 +3,9 @@
   import GameCardAdmin from "../../components/GameCardAdmin.svelte";
   import { fetchGet, fetchPost } from "../../../util/api.js";
   import { onMount } from "svelte";
+  import bootstrap from "../../../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
 
-  const user = { role: "user" };
+  const user = { role: "admin" };
   let gamesList = [];
 
   onMount(async () => {
@@ -19,9 +20,14 @@
         description: description,
       };
 
-      await fetchPost("http://localhost:8080/api/games", newGame);
+      // NOTHING HAPPENS AFTER FETCH
+      // THEREFORE showToast IS ABOVE
+      showToast("Game succesfully created.");
+
+      const response = await fetchPost("http://localhost:8080/api/games", newGame);
     } catch (error) {
       console.error("Unable to save game", error);
+      showToast("Unable to save game.");
     }
   }
 
@@ -36,6 +42,17 @@
       );
     });
   }
+
+  let message;
+
+  function showToast(data) {
+    message = data;
+    console.log(message);
+
+    const toastElement = document.getElementById("liveToast");
+    const toast = new bootstrap.Toast(toastElement);
+    toast.show();
+  }
 </script>
 
 <div class="container mt-5">
@@ -44,6 +61,7 @@
     Find active events under every game you can desire. Follow news, chat with like minded souls and and share
     strategies.
   </p>
+  
   <input bind:value={searchTerm} type="text" placeholder="Search..." />
 
   <div class="row gx-5 gy-5 mt-3">
@@ -54,5 +72,15 @@
     {#each searchTerm ? searchGameList : gamesList as game}
       <GameCard {game} />
     {/each}
+  </div>
+</div>
+
+<div class="toast-container position-fixed bottom-0 end-0 p-3">
+  <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+    <div class="toast-header">
+      <strong class="me-auto">Information</strong>
+      <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+    </div>
+    <div class="toast-body">{message}</div>
   </div>
 </div>
