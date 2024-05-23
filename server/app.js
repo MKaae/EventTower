ec787b46f039c90a5e71ba5091c4db13d9911d87
zivbabs <117026503+zivbabs@users.noreply.github.com>
2024-05-23 09:36:36 +0200

    small commit serverside creation

---
+++ server/app.js
@@ -0,0 +1,26 @@
+import "dotenv/config";
+import express from "express";
+const app = express();
+
+import path from "path";
+app.use(express.static(path.resolve("../client/dist")))
+
+import livereload from "livereload";
+import connectLivereload from "connect-livereload";
+
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch("../client/dist");
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 500);
+});
+
+app.use(connectLivereload());
+
+app.get("/test", (req,res) => {
+    res.send({data:"test123"})
+})
+
+const PORT = process.env.PORT ?? 8080;
+app.listen(PORT, () => console.log("Server is running on:", PORT))
