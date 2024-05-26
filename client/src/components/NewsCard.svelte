423e064c5c1d4d1580e25224a6f587702fe476a3
Steffen Grøn Andersen <steffengroenandersen@gmail.com>
2024-05-26 11:14:21 +0200

    Add news site

---
+++ client/src/components/NewsCard.svelte
@@ -0,0 +1,8 @@
+<script>
+  export let news;
+</script>
+
+<div class="m-5 p-5 bg-light">
+  <h1>{news.title}</h1>
+  <p>{news.body}</p>
+</div>
--- client/src/pages/Event/Event.svelte
+++ client/src/pages/Event/Event.svelte
@@ -5,25 +5,37 @@
   import { eventPage } from "../../stores/generalStore.js";
   import { eventStore } from "../../stores/generalStore.js";
   import { locationStore } from "../../stores/locationStore.js";
+  import NewsCard from "../../components/NewsCard.svelte";
 
   const params = useParams();
   const eventId = $params.id;
   let event = {};
-
-
-  
+  let newsList = [];
 
   onMount(async () => {
+    locationStore.update();
     event = await fetchGet(`http://localhost:8080/api/events/${eventId}`);
     eventPage.set(true);
     eventStore.set({ id: event._id });
-    locationStore.update();
-    console.log($locationStore);
-
+    newsList = event.news;
   });
 </script>
 
-<div class="container mt-5">
-  <div class=""><h1>{event.name}</h1></div>
-  <div class=""><h2>News</h2></div>
+<div class="container-fluid mt-5 d-flex flex-column justify-content-center align-items-center">
+  <div class="row d-flex justify-content-center">
+    <div class="col-12 col-lg-6">
+      <h1 class="text-center">{event.name}</h1>
+      <p class="text-center">{event.description}</p>
+    </div>
+
+    <div class="col-12 col-lg-6" style="max-width: 500px; max-height: auto;">
+      <img src={event.imageURL} alt="event" class="img-fluid rounded" />
+    </div>
+  </div>
+  <div class="m-5"><h2>News</h2></div>
+  <div>
+    {#each newsList as news}
+      <NewsCard {news} />
+    {/each}
+  </div>
 </div>
