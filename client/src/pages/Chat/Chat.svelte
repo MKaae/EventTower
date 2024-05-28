a0b787462214daff568a999f73a63bcd09927bc2
zivbabs <117026503+zivbabs@users.noreply.github.com>
2024-05-28 23:35:50 +0200

    fixed major chat thing, added favicon

--- client/src/pages/Chat/Chat.svelte
+++ client/src/pages/Chat/Chat.svelte
@@ -31,15 +31,27 @@
         }
     });
 </script>
+<div class="d-flex flex-column justify-content-center align-items-center m-2">
+    <h6>Chatroom</h6>
+    {#if $user === null}
+        <h1>You are not logged in.</h1>
+        <p>Please login to see chat.</p>
+    {:else}
+    {#each messages as message}
+        <p>{message.user} says: {message.message}</p>
+        {/each}
+    <div class="d-flex flex-column">
+      <textarea class="textbox mb-2" bind:value={message}></textarea>
+      <button class="btn btn-primary" on:click={submitMessage}>Submit Message</button>
+    </div>
+    {/if}
+</div>
 
-<h6>Chatroom</h6>
-{#if $user === null}
-    <h1>You are not logged in.</h1>
-    <p>Please login to see chat.</p>
-{:else}
-{#each messages as message}
-    <p>{message.user} says: {message.message}</p>
-{/each}
-<input type="text" bind:value={message}>
-<button on:click={submitMessage}>Submit Message</button>
-{/if}
+<style>
+    .textbox {
+        width: 200px;
+    }
+</style>
+
+
+
